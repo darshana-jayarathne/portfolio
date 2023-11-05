0f4ee4d11808424a937ca444924b05aebd5c95d4
Darshana Jayarathna <jayarathnadarshana96@gmail.com>
2023-11-05 19:37:37 +0530

    last-commit

---
+++ components/SiteFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="bg-gray-800">
+        <section class="mx-auto max-w-7xl px-2 sm:px-4 lg:py-6 text-gray-300 flex flex-row">
+            <p class="flex-1">
+                &copy;
+                Your name 2023. All rights reserved.
+            </p>
+            <div class="flex flex-row justify-center gap-4 mb-4 mt-2">
+
+            </div>
+        </section>
+    </footer>
+</template>
+
+<script setup>
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content',
+    '@nuxtjs/apollo',
+  ],
+  runtimeConfig: {
+    githubToken: process.env.GITHUB_TOKEN
+  },
+  content: {
+    highlight: {
+      theme: 'nord',
+      preload: ['ts','js','css','java','json','bash','vue'],
+    }
+  },
+  apollo: {
+    clients: {
+      default: {
+        tokenName: 'github-token',
+        httpEndpoint: 'https://api.github.com/graphql'
+      }
+    },
+  },
 })
